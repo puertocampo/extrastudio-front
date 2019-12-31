ff24ee2d44cfd58cc93f4633bc15b6c2551dc1b5
puertocampo <puertocampo@gmail.com>
2019-12-31 16:12:45 +0900

    add 左右スワイプできるようになった

--- screens/SelectionScreen.js
+++ screens/SelectionScreen.js
@@ -1,111 +1,226 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Button, AsyncStorage } from "react-native";
+import styled from 'styled-components/native'
+import { css } from 'styled-components'
+import { StyleSheet, Text, View, Button, AsyncStorage, Dimensions, Animated, Image, PanResponder } from "react-native";
 import Firebase from "../firebase";
-// import { IUser } from "./type/user";
 
-// interface IState {
-//   isLoggedIn: Boolean;
-//   user: IUser;
-// }
+const events = [
+  { eventId: "event01", uri: require('../assets/image/baseball.jpeg') },
+  { eventId: "event02", uri: require('../assets/image/halloween.jpeg') },
+  { eventId: "event03", uri: require('../assets/image/soccer.jpeg') },
+  { eventId: "event04", uri: require('../assets/image/theater.jpeg') },
+  { eventId: "event05", uri: require('../assets/image/togei.jpeg') },
+];
 
-// export default class LoginScreen extends Component<any, IState> {
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+const Wrapper = styled.View`
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  /* background-color: rgb(135, 135, 135); */
+  background-color: rgba(0, 0, 0, 0.4);
+  position: absolute;
+  left: 0;
+  top: 0;
+`;
+
+const ButtonContainer = styled.View`
+  height: 110;
+`;
+
 export default class SelectionScreen extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+    this.position = new Animated.ValueXY();
     this.state = {
-      isLoggedIn: false,
-      user: {
-        email: "",
-        name: ""
-      }
+      currentIndex: 0
     };
-    console.log("constructor");
-    try {
-      // firebase 操作の初期化 と Firebase class のインスタンス化
-      Firebase.firebaseInitialize();
-      console.log("firebaseInitialize");
-    } catch {
-      console.log("release");
-    }
-  }
-
-  // async componentDidMount() {
-  //   const user = await Firebase.fetchCurrentUser();
-  //   this.setState({
-  //     isLoggedIn: !!user,
-  //     user: {
-  //       email: user ? user.email : "",
-  //       name: user ? user.displayName : ""
-  //     }
-  //   });
-  //   console.log("componentDidMount", user);
-  // }
-
-  async componentDidMount() {
-    this.fetchCurrentLoggedInUser();
-  }
-
-  fetchCurrentLoggedInUser = () => {
-    Firebase.fetchCurrentUser().then(firebaseUser => {
-      this.setState({
-        isLoggedIn: !!firebaseUser,
-        user: {
-          email: firebaseUser ? firebaseUser.email : "",
-          name: firebaseUser ? firebaseUser.displayName : ""
-        }
-      });
+    this.rotate = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: ['-10deg', '0deg', '10deg'],
+      extrapolate: 'clamp'
     });
-  };
+    this.likeOpacity = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: [0, 0, 1],
+      extrapolate: 'clamp'
+    });
+    this.nopeOpacity = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: [1, 0, 0],
+      extrapolate: 'clamp'
+    });
+    this.nextCardOpacity = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: [1, 0, 1],
+      extrapolate: 'clamp'
+    });
+    this.nextCardScale = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: [1, 0.8, 1],
+      extrapolate: 'clamp'
+    });
+  }
 
-  handleLogIn = () => {
-    Firebase.handleLogIn().then(
-      // (firebaseUser: firebase.auth.UserCredential) => {
-      firebaseUser => {
-        this.setState({
-          isLoggedIn: true,
-          user: {
-            email: firebaseUser.user.email,
-            name: firebaseUser.user.displayName
+  componentWillMount() {
+    this.PanResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderMove: (evt, gestureState) => {
+        this.position.setValue({ x: gestureState.dx, y: gestureState.dy });
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+        if (gestureState.dx > 120) {
+          Animated.spring(this.position, {
+            toValue: { x: SCREEN_WIDTH + 100, y: gestureState.dy }
+          }).start(() => {
+            this.setState({ currentIndex: this.state.currentIndex + 1 }, () => {
+              this.position.setValue({ x: 0, y: 0 })
+            })
+          })
+        } else if (gestureState.dx < -120) {
+          Animated.spring(this.position, {
+            toValue: { x: -SCREEN_WIDTH - 100, y: gestureState.dy }
+          }).start(() => {
+            this.setState({ currentIndex: this.state.currentIndex + 1 }, () => {
+              this.position.setValue({ x: 0, y: 0 })
+            })
+          })
+        } else {
+          Animated.spring(this.position, {
+            toValue: { x: 0, y: 0 },
+            friction: 4
+            }).start()
           }
-        });
-      })
-  };
-
-  handleLogout = () => {
-    Firebase.handleLogOut().then(() => {
-      this.setState({
-        isLoggedIn: false,
-        user: {
-          email: "",
-          name: ""
-        }
-      });
+      }
     });
+    this.rotateAndTranslate = {
+      transform: [
+        {
+          rotate: this.rotate
+        },
+        ...this.position.getTranslateTransform()
+        ]
+    };
+}
+
+  renderEventCards = () => {
+    return events.map((item, index) => {
+      console.log(item);
+      if (index < this.state.currentIndex) {
+        return null;
+      } else if (index === this.state.currentIndex) {
+        return (
+          <Animated.View
+            {...this.PanResponder.panHandlers}
+            key={item.eventId}
+            style={[this.rotateAndTranslate,
+              {
+                height: SCREEN_HEIGHT - 170,
+                width: SCREEN_WIDTH,
+                padding: 10,
+                position: "absolute",
+                left: 0
+              }
+            ]}
+          >
+            <Animated.View
+              style={{
+                opacity: this.likeOpacity,
+                transform: [{ rotate: "-30deg" }],
+                position: "absolute",
+                top: 50,
+                left: 40,
+                zIndex: 1000
+              }}
+            >
+              <Text
+                style={{
+                  borderWidth: 1,
+                  borderColor: "green",
+                  color: "green",
+                  fontSize: 32,
+                  fontWeight: "800",
+                  padding: 10
+                }}
+              >
+                LIKE
+              </Text>
+            </Animated.View>
+            <Animated.View
+              style={{
+                opacity: this.nopeOpacity,
+                transform: [{ rotate: "30deg" }],
+                position: "absolute",
+                top: 50,
+                right: 40,
+                zIndex: 1000
+              }}
+            >
+              <Text
+                style={{
+                  borderWidth: 1,
+                  borderColor: "red",
+                  color: "red",
+                  fontSize: 32,
+                  fontWeight: "800",
+                  padding: 10
+                }}
+              >
+                NOPE
+              </Text>
+            </Animated.View>
+            <Image
+              style={{
+                flex: 1,
+                height: null,
+                width: null,
+                resizeMode: "cover",
+                borderRadius: 20
+              }}
+              source={item.uri}
+            />
+          </Animated.View>
+        );
+      } else {
+        return (
+          <Animated.View
+            key={item.eventId}
+            style={{
+              opacity: this.nextCardOpacity,
+              transform: [{ scale: this.nextCardScale }],
+              height: SCREEN_HEIGHT - 170,
+              width: SCREEN_WIDTH,
+              padding: 10,
+              position: "absolute",
+              left: 0
+            }}
+          >
+            <Image
+              style={{
+                flex: 1,
+                height: null,
+                width: null,
+                resizeMode: "cover",
+                borderRadius: 20
+              }}
+              source={item.uri}
+            />
+          </Animated.View>
+        );
+      }
+    }).reverse();
   };
 
   render() {
     return (
-      <View style= { styles.container } >
-      <Button
-        onPress={ this.handleLogIn }
-    title = "Login With Google"
-    color = "#ff0000"
-      />
-    {
-      this.state.isLoggedIn && (
-        <View>
-        <Text>
-        Hello, { this.state.user.name }({ this.state.user.email })
-        </Text>
-        < Button
-          onPress={ this.handleLogout }
-          title="Logout from Google"
-          color="#0000ff"
-      />
-      </View>
-      )
-    }
-      </View>
+      <Wrapper>
+        <View style={{ flex: 1, marginTop: 60 }}>
+          {this.renderEventCards()}
+        </View>
+        <ButtonContainer><Text>あ</Text></ButtonContainer>
+      </Wrapper>
     );
   }
 }
@@ -113,8 +228,18 @@ export default class SelectionScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    // backgroundColor: "#000000",
+    backgroundColor: "rgb(135, 135, 135)",
+    width: "100%",
+    paddingTop: 15,
+    paddingRight: 5,
+    paddingBottom: 7,
+    paddingLeft: 5,
+    // padding: "15% 1% 7% 1%"
+    // "73px 1px 30px"
     alignItems: "center",
-    justifyContent: "center"
+    flexDirection: "row"
+    // justifyContent: "center"
   }
 });
+

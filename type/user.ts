5c67b9b96999f61f0d74e8f866befadaa06cd67f
puertocampo <puertocampo@gmail.com>
2020-01-11 16:46:48 +0900

    update user type

--- type/user.ts
+++ type/user.ts
@@ -1,4 +1,10 @@
 export interface IUser {
+  userId: string;
   email: string;
   name: string;
+  birthDate: Date;
+  sex: string;
+  profession: string;
+  address: string;
+  interests: string[];
 }

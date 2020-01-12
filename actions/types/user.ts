9fd27edda0b49c25a17f6c642dc376b035c306b5
puertocampo <puertocampo@gmail.com>
2020-01-12 15:36:56 +0900

    update comment

--- actions/types/user.ts
+++ actions/types/user.ts
@@ -1,15 +1,15 @@
-// 統合処理用の actionType
+// 統合処理用
 export const LOGIN = 'login';
 export const LOGOUT = 'logout';
 export const REGISTER_USER = 'register_user';
 export const UPDATE_USER = 'update_user';
 export const WITHDRAW_USER = 'withdraw_user';
 
-// state更新用の actionType
+// state更新
 export const SET_USER = 'set_user';
 export const INITIALIZE_USER = 'initialize_user';
 
-// DB更新用の actionType
+// DB処理
 export const POST_USER = 'post_user';
 export const FETCH_USER = 'fetch_user';
 export const PUT_USER = 'put_user';

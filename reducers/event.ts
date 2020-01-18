efa82eb38454497376ec7c5589df3332a525a602
puertocampo <puertocampo@gmail.com>
2020-01-18 16:50:34 +0900

    fix eventsのstoreの持ち方

--- reducers/event.ts
+++ reducers/event.ts
@@ -2,15 +2,15 @@ import {
   UPDATE_EVENTS
 } from '../actions/types/event';
 import { IEvent } from "../type/event";
+import { actionChannel } from 'redux-saga/effects';
 
 const INITIAL_STATE: IEvent[] = [];
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case UPDATE_EVENTS:
-      return {
-        ...state, ...action.payload
-      };
+      // console.log('UPDATE_EVENTS', action.payload);
+      return action.payload;
 
     default:
       return state;

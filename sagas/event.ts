f300986824e365890ede02aae51734658a33081b
puertocampo <puertocampo@gmail.com>
2020-01-18 16:51:31 +0900

    update events sagaを整備

--- sagas/event.ts
+++ sagas/event.ts
@@ -1,29 +1,23 @@
-import { takeEvery, call, put, select } from 'redux-saga/effects';
+import { takeLatest, call, put, select } from 'redux-saga/effects';
 import Api from "../api";
 import {
   FETCH_EVENTS
 } from '../actions/types/event';
 import { updateEvents } from "../actions";
-// import { getIdToken } from "../selectors";
 import { getStorage } from "./asyncStorage";
 
 function* fetchEvents() {
-  // const idToken = yield select(getIdToken);
-  const str = yield call(getStorage, ['userId', 'idToken']);
-  const idToken = 'a';
+  const { idToken } = yield call(getStorage, ['idToken']);
   const { events, err } = yield call(Api.fetchEvents, idToken);
-  // const events = yield call(Api.fetchEvents, idToken);
-  console.log('events', events);
   if (events && events.length) {
     yield put(updateEvents(events));
   } else {
-    // console.log('else', err);
     yield put(updateEvents([]));
   }
 }
 
 const watchEventAsync = [
-  takeEvery(FETCH_EVENTS, fetchEvents)
+  takeLatest(FETCH_EVENTS, fetchEvents)
 ];
 
 export default watchEventAsync;

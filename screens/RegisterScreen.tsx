8e19c267b566bb2e27ba6706bf3d762d3a7cd1f8
puertocampo <puertocampo@gmail.com>
2020-03-22 21:59:45 +0900

    add 色々機能追加

--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, Platform, TextInput, DatePickerIOS, Picker, ScrollView } from "react-native";
-import { Button } from "react-native-elements";
+import { Button, CheckBox } from "react-native-elements";
 import Constants from 'expo-constants';
+import { SexRadioButtonList } from "../components/molecules";
 
 import { connect } from 'react-redux';
 import * as actions from '../actions';
@@ -14,6 +15,7 @@ interface IProps { };
 interface IState {
   name: string;
   birthDate: Date;
+  sex: string;
   profession: string;
 }
 
@@ -48,6 +50,7 @@ class RegisterScreen extends Component<IProps, IState> {
     this.state = {
       name: "",
       birthDate: new Date(),
+      sex: "",
       profession: ""
     }
   }
@@ -65,12 +68,17 @@ class RegisterScreen extends Component<IProps, IState> {
     this.setState({ birthDate: date });
   }
 
+  handleChangeSex = (sex: string) => {
+    this.setState({ sex });
+  }
+
   handleChangeProfession = (profession: string) => {
     this.setState({ profession });
   }
 
   render() {
-    const { name, birthDate, profession } = this.state;
+    const { name, birthDate, sex, profession } = this.state;
+    const birthDateJa = `${birthDate.getFullYear()}年 ${birthDate.getMonth() + 1}月 ${birthDate.getDate()}日`;
     return (
       <ScrollView style={styles.wrapper}>
         <Text
@@ -78,18 +86,32 @@ class RegisterScreen extends Component<IProps, IState> {
         >
           ニックネーム
         </Text>
-        <TextInput onChangeText={this.handleChangeName}
-          value={name} placeholder="入力してください" />
+        <TextInput
+          style={styles.formInput}
+          onChangeText={this.handleChangeName}
+          value={name}
+          placeholder="入力してください"
+        />
         <Text
           style={styles.formTitle}
         >
           生年月日
         </Text>
+        <TextInput
+          style={styles.formInput}
+          value={birthDateJa}
+          placeholder="日付を選択してください"
+          onFocus={() => {
+            console.log("focus");
+          }}
+        />
         {
           isIos ?
             <DatePickerIOS
               date={birthDate}
               onDateChange={this.handleChangeDate}
+              mode="date"
+              locale="ja"
             /> : null
         }
         <Text
@@ -97,6 +119,11 @@ class RegisterScreen extends Component<IProps, IState> {
         >
           性別
         </Text>
+        <SexRadioButtonList
+          sex={sex}
+          handleChangeSex={this.handleChangeSex}
+          style={{ marginBottom: 20 }}
+        />
         <Text
           style={styles.formTitle}
         >
@@ -122,11 +149,27 @@ class RegisterScreen extends Component<IProps, IState> {
         >
           興味のあるジャンル
         </Text>
+        <CheckBox
+          title="家族"
+          checked={true}
+          containerStyle={styles.formCheckBox}
+          checkedColor="#4D7DF9"
+        />
         <Text
           style={styles.formTitle}
         >
           興味のあるワード
         </Text>
+        <Picker
+          selectedValue={profession}
+          onValueChange={this.handleChangeProfession}
+        >
+          {
+            professionItems.map(profession => {
+              return <Picker.Item label={profession.label} value={profession.value} />
+            })
+          }
+        </Picker>
         <View style={styles.registerButtonContainer}>
           <Button
             title="登録"
@@ -179,14 +222,29 @@ const styles = StyleSheet.create({
   formTitle: {
     fontSize: isIos ? 18 : 12,
     fontWeight: "bold",
-    marginBottom: 20,
-    color: "#707070"
+    color: "#707070",
+    marginBottom: 10
   },
   formInput: {
     fontSize: isIos ? 18 : 12,
     width: "100%",
-    height: 40,
-    borderColor: "#CDD6DD"
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#CDD6DD",
+    paddingLeft: 20,
+    paddingTop: 12,
+    paddingBottom: 12,
+    marginBottom: 20
+  },
+  formRadioButton: {
+    width: 100,
+    backgroundColor: "#fff",
+    borderColor: "#fff"
+  },
+  formCheckBox: {
+    width: 100,
+    backgroundColor: "#fff",
+    borderColor: "#fff"
   },
   registerButtonContainer: {
     width: "100%",
